f41e6ce038c449fb75caa169796a47b434a400ac
unknown <74738757+leonid-br@users.noreply.github.com>
2022-05-18 01:33:34 +0300

    part 8
    затык с редакс не решен

--- src/redux/order/order-reducer.js
+++ src/redux/order/order-reducer.js
@@ -4,23 +4,38 @@ import actions from './order-actions';
 
 const order = createReducer([], {
     // [actions.addDish]: (state, { payload }) => [...state, payload],
-    [actions.addDish]: (state, { payload }) => {
-        console.log(payload);
-        return state.length === 0
-            ? [{ ...payload, ammount: 1 }]
-            : [
-                  state.reduce(
-                      (acc, cur) =>
-                          cur.id !== payload.id
-                              ? { ...acc, ...payload, ammonut: 1 }
-                              : [
-                                    ...state,
-                                    { ...payload, ammount: 1 },
-                                ],
-                      state,
-                  ),
-              ];
-    },
+    [actions.addDish]: (state, { payload }) => [
+        ...state,
+        { ...payload, ammount: 1 },
+    ],
+
+    //     return state.length === 0
+    //         ? [{ ...payload, ammount: 1 }]
+    //         : state.reduce(
+    //               (acc, cur) => {
+    //                   if (cur.id !== payload.id) {
+    //                       console.log(
+    //                           'айди НЕ равны',
+    //                           cur.id !== payload.id,
+    //                       );
+    //                       return [
+    //                           ...state,
+    //                           { ...payload, ammount: 1 },
+    //                       ];
+    //                   } else {
+    //                       console.log(
+    //                           'айди равны',
+    //                           cur.id !== payload.id,
+    //                       );
+    //                       const newCur = { ...cur, ammount: 2 };
+    //                       state;
+    //                       return [...state, { ...cur, ammount: 2 }];
+    //                   }
+    //               },
+
+    //               state,
+    //           );
+    // },
 });
 
 export default combineReducers({ order });
